90ba56070e43880376058b07325e667dc2f22105
Juan Guevara M. <juanexgue@gmail.com>
2025-08-04 17:16:13 -0500

    init commit others

---
+++ src/main/java/com/juanex/solidapp/SolidAppApplication.java
@@ -0,0 +1,13 @@
+package com.juanex.solidapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolidAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolidAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanex/solidapp/SolidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanex.solidapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
